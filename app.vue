5433ea18913e648de78181b77bad5661ce09b2b7
sadi <sheaksadi123456@gmail.com>
2024-04-20 20:55:25 +0600

    added favicon and changed few things

--- app.vue
+++ app.vue
@@ -3,6 +3,8 @@ import SectionHeader from "~/components/section-header.vue";
 import Contacts from "~/components/contacts.vue";
 import About from "~/components/about.vue";
 
+
+
 const sections = [
   {
     title: "About",
@@ -79,7 +81,7 @@ const onSectionClick = (sectionId: string) => {
           <div class="w-full text-gray-500 select-none">
             <h1 class="text-6xl font-bold text-gray-200">Sheak Sadi</h1>
             <h2 class="text-2xl mt-3 text-gray-400">Software Developer</h2>
-            <h2 class=" mt-3 ">Currently working part time at <br><a class="hover:text-gray-300" href="https://www.netpoint-media.de/">Net Point Media GMBH</a></h2>
+            <h2 class=" mt-3 transition-all duration-500">Currently working part time at <br><a class="hover:text-gray-300 " href="https://www.netpoint-media.de/" target="_blank">Net Point Media GMBH</a></h2>
           </div>
           <div class="w-full ">
             <SectionHeader
@@ -111,6 +113,7 @@ const onSectionClick = (sectionId: string) => {
       </div>
 
     </div>
+    <Title>Sheak Sadi</Title>
     <div ref="blob" id="blob"></div>
     <div id="blur"></div>
   </div>
